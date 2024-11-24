8a7273e5b24d3ef5dd8882e940565852fe0f7645
EeveeSwordx <eeveeswordx@gmail.com>
2024-11-24 09:53:24 -0500

    Update moves.ts

--- data/mods/infernalborn/moves.ts
+++ data/mods/infernalborn/moves.ts
@@ -1,1 +1,15 @@
+'use strict';
 
+exports.Moves = {
+  bludgeonblow: {
+    num: 2001,
+    accuracy: 90,
+    basePower: 120,
+    category: 'Physical',
+    priority: 0,
+    pp: 5,
+    type: 'Fighting',
+    target: 'normal',
+    effect: 'Deals massive physical damage to the target. The power of this move halves with every consecutive use or if the opponent has more than 50% HP.',
+  },
+};
