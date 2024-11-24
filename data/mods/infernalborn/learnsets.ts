744ba61d393c9a94a37c03057e3e98a4b2defb05
EeveeSwordx <eeveeswordx@gmail.com>
2024-11-24 09:54:29 -0500

    Create learnsets.ts

---
+++ data/mods/infernalborn/learnsets.ts
@@ -0,0 +1,10 @@
+export const Learnsets: {[k: string]: ModdedLearnsetData} = {
+  ankyladus: {
+    learnset: {
+      tackle: ["8L1"],
+      irondefense: ["8L1"],
+      earthquake: ["8M"],
+      megatonstrike: ["8L1"], // Custom move
+    },
+  },
+};
