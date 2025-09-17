8f16b99f363832bd45c31a0cd3d8a6002fe99018
EeveeSwordx <eeveeswordx@gmail.com>
2025-09-16 22:34:42 -0400

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -11,7 +11,6 @@ exports.Formats = [
     ruleset: ['Standard'], // Optional: Define rules like 'Standard', 'Dynamax Clause', etc.
     banlist: ['Uber'], // Optional: Specify banned elements
   },
-  
   {
     name: "Monotype",
     desc: "Only one type allowed",
@@ -19,10 +18,17 @@ exports.Formats = [
     ruleset: ['Standard', 'Dynamax Clause'], // Optional: Define rules
     banlist: ['Shadow Tag', 'Baton Pass'], // Optional: Specify banned elements
   },
-
-{
+  {
+    name: "Random Battle",
+    desc: "Randomized Teams",
+    mod: 'infernalborn', // Folder name in `mods/` directory for this mod
+   team: 'random',
+    ruleset: ['Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'], // Optional: Define rules
+    banlist: ['Shadow Tag', 'Baton Pass'], // Optional: Specify banned elements
+  },
+  {
     name: "Anything Goes",
-    desc: "No rules!",
+    desc: "Basically no rules",
     mod: 'infernalborn', // Folder name in `mods/` directory for this mod
   },
 ];
