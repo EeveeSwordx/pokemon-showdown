348d3a1688c75a0d104d9e02a9b547e037c46b61
EeveeSwordx <eeveeswordx@gmail.com>
2025-09-16 22:19:12 -0400

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,19 +1,25 @@
 exports.Formats = [
   // First custom mod
   {
-    name: "[Gen 9] Infernal Fakemon",
+    name: "Roleplay Standard",
     desc: "Custom Fakemon from Infernal.",
     mod: 'infernalborn', // Folder name in `mods/` directory for this mod
     ruleset: ['Standard'], // Optional: Define rules like 'Standard', 'Dynamax Clause', etc.
-    banlist: ['Uber', 'Moody', 'Power Construct', 'Arena Trap'], // Optional: Specify banned elements
+    banlist: ['Uber'], // Optional: Specify banned elements
   },
   
-  // Second custom mod
   {
-    name: "[Gen 9] Is this showing up",
-    desc: "Another custom Pokémon mod with unique Pokémon.",
+    name: "Monotype",
+    desc: "Only one type allowed",
     mod: 'gen1rbycap', // Folder name in `mods/` directory for this mod
     ruleset: ['Standard', 'Dynamax Clause'], // Optional: Define rules
     banlist: ['Shadow Tag', 'Baton Pass'], // Optional: Specify banned elements
+  },
+
+{
+    name: "Anything Goes",
+    desc: "No rules!",
+    mod: 'gen1rbycap', // Folder name in `mods/` directory for this mod
+    ruleset: ['Standard', 'Dynamax Clause'], // Optional: Define rules
   },
 ];
